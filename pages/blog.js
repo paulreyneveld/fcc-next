cc5a02eab7939a90c6cae67d9ade5db7527fe953
Paul <63985305+paulreyneveld@users.noreply.github.com>
2021-05-11 16:21:46 -0700

    Added a Link component

--- pages/blog.js
+++ pages/blog.js
@@ -1,11 +1,19 @@
+import Link from 'next/link'
 import posts from '../posts.json'
 
 const Blog = () => (
   <div>
     <h1>Blog</h1>
+
     <ul>
       {Object.entries(posts).map((value, index) => {
-        return <li key={index}>{value[1].title}</li>
+        return (
+          <li key={index}>
+            <Link href='/blog/[id]' as={'/blog/' + value[0]}>
+              <a>{value[1].title}</a>
+            </Link>
+          </li>
+        )
       })}
     </ul>
   </div>
