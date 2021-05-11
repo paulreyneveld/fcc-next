bd5f7671a62821acbfa6c6093610de2530ee0766
Paul <63985305+paulreyneveld@users.noreply.github.com>
2021-05-11 16:11:27 -0700

    Updated ID component to match tutorial

--- pages/blog/[id].js
+++ pages/blog/[id].js
@@ -3,14 +3,12 @@ import posts from '../../posts.json'
 
 const ID = () => {
   const router = useRouter()
-  console.log(router.query.id) 
   const post = posts[router.query.id]
-  console.log(posts)
-  console.log(post)
+  
   return (
     <>
-      <h1></h1>
-      <p>{post.title}</p>
+      <h1>{post.title}</h1>
+      <p>{post.content}</p>
     </>
   )
 }
