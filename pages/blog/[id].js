6385ba9362d901e47799a180679a5dd151d519bc
Paul <63985305+paulreyneveld@users.noreply.github.com>
2021-05-11 16:12:41 -0700

    Changed [id].js to match tutorial exactly

--- pages/blog/[id].js
+++ pages/blog/[id].js
@@ -1,10 +1,12 @@
 import { useRouter } from 'next/router'
 import posts from '../../posts.json'
 
-const ID = () => {
+export default () => {
   const router = useRouter()
+
   const post = posts[router.query.id]
-  
+  if (!post) return <p></p>
+
   return (
     <>
       <h1>{post.title}</h1>
@@ -12,5 +14,3 @@ const ID = () => {
     </>
   )
 }
-
-export default ID
